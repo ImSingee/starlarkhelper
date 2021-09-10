42ecb1785b4fed12b2a322465c29779b19b3acff
Singee <git@singee.me>
2021-09-10 15:54:34 +0800

    add AssertableBool

--- assert.go
+++ assert.go
@@ -1,11 +1,24 @@
 package starlarkhelper
 
+import (
+	"go.starlark.net/starlark"
+)
+
 // Assertable 为数据提供 assert 的能力
 type Assertable interface {
 	Assert() (pass bool, failReason string)
 }
 
-// AssertFunc TODO
-func AssertFunc() {
+type AssertableBool struct {
+	starlark.Bool
 
+	Reason string
+}
+
+func (a AssertableBool) Assert() (pass bool, failReason string) {
+	return bool(a.Bool), a.Reason
+}
+
+func NewFalseWithReason(reason string) AssertableBool {
+	return AssertableBool{false, reason}
 }
