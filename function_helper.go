776ceea8d0a15e4e7e95959eda00f78c2a1da108
Singee <git@singee.me>
2022-05-27 15:55:51 +0800

    remove UnpackBasicArgs

--- function_helper.go
+++ function_helper.go
@@ -54,64 +54,9 @@ func (h *Helper) UnpackPositionalArgs(min int, vars ...interface{}) *Helper {
 	})
 }
 
-// UnpackBasicArgs 类似于 UnpackArgs, 但仅支持 int, int64, uint64, string, bool
+// Deprecated: UnpackBasicArgs  please use UnpackArgs directly
 func (h *Helper) UnpackBasicArgs(pairs ...interface{}) *Helper {
-	return h.withErrorHandler(func() error {
-		ppp := make([][3]interface{}, len(pairs)/2)
-		for i := 0; i < len(ppp); i++ {
-			ppp[i][0] = pairs[i*2]   // 名称
-			ppp[i][1] = pairs[i*2+1] // 指向基本类型的指针
-			switch v := ppp[i][1].(type) {
-			case *int:
-				ppp[i][2] = starlark.MakeInt(*v)
-			case *int64:
-				ppp[i][2] = starlark.MakeInt64(*v)
-			case *uint64:
-				ppp[i][2] = starlark.MakeUint64(*v)
-			case *string:
-				ppp[i][2] = starlark.String(*v)
-			case *bool:
-				ppp[i][2] = starlark.Bool(*v)
-			default:
-				return errors.Errorf("unsupported type %T", v)
-			}
-			pairs[i*2+1] = &ppp[i][2] // 指向 starlark 类型的指针
-		}
-
-		err := starlark.UnpackArgs(h.Name, h.PositionalArgs, h.Kwargs, pairs...)
-		if err != nil {
-			return err
-		}
-		for i := 0; i < len(ppp); i++ {
-			switch v := ppp[i][1].(type) {
-			case *int:
-				vv, ok := ppp[i][2].(starlark.Int).Int64()
-				if !ok {
-					return errors.Errorf("arg %s: overflow int64", ppp[i][0])
-				}
-				*v = int(vv)
-			case *int64:
-				vv, ok := ppp[i][2].(starlark.Int).Int64()
-				if !ok {
-					return errors.Errorf("arg %s: overflow int64", ppp[i][0])
-				}
-				*v = vv
-			case *uint64:
-				vv, ok := ppp[i][2].(starlark.Int).Uint64()
-				if !ok {
-					return errors.Errorf("arg %s: overflow uint64", ppp[i][0])
-				}
-				*v = vv
-			case *string:
-				*v = string(ppp[i][2].(starlark.String))
-			case *bool:
-				*v = bool(ppp[i][2].(starlark.Bool))
-			default: // impossible
-			}
-		}
-
-		return nil
-	})
+	return h.UnpackArgs(pairs...)
 }
 
 // GetKeywordArgs 从 keyword args 中提取出指定 key 对应的值，如果值不存在返回 nil
